e757d1cc889e68c839e9bc4b5e6b5d7aafa7cb07
Nateyana <nblake707@gmail.com>
2020-08-07 13:09:07 -0400

    finished slideshow

--- js/slides.js
+++ js/slides.js
@@ -7,29 +7,36 @@ let z = 1;
 
 // Change slide based on z-index when slide area is clicked
 slideArea.addEventListener("click", () => {
-    currentSlide = currentSlide + 1;
-    z = z + 1;
+  currentSlide = currentSlide + 1;
+  z = z + 1;
 
-    if (currentSlide > images.length -1){
-        currentSlide = 0;
-    }
+  if (currentSlide > images.length - 1) {
+    currentSlide = 0;
+  }
 
-    // remove the animation from the style for every image
-    images.forEach(image => {
-      image.style.animation = "";  
-    })
+  // remove the animation from the style for every image
+  images.forEach((image) => {
+    image.style.animation = "";
+  });
 
-    //pick the right image
-    images[currentSlide].style.zIndex = z;
-    images[currentSlide].style.animation = "fade 0.5s";
-})
+  //pick the right image
+  images[currentSlide].style.zIndex = z;
+  images[currentSlide].style.animation = "fade 0.5s";
+});
 
 slideArea.addEventListener("mouseover", () => {
-    images.forEach(image => {
-        const x = 100 * Math.random();
-        const y = 100 * Math.random();
-    
-        image.style.transform = `translate(${x}px,${y}px)`;
-    })
-})
+  images.forEach((image) => {
+    const x = 25 * Math.floor(Math.random() * 5) - 50;
+    const y = 25 * Math.floor(Math.random() * 5) - 50;
 
+    image.style.transform = `translate(${x}px,${y}px)`;
+  });
+});
+
+// put images back into place when mouse is moved away
+slideArea.addEventListener("mouseout", () => {
+  images.forEach((image) => {
+    //remove transform
+    image.style.transform = "";
+  });
+});
